66223420982be65300bbf1d9d45acccd85cb95a5
littlelionstudios <flavorformulas@gmail.com>
2023-02-22 10:19:15 -0500

    Final

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// Menu data structure
-var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+// https://perscholas.instructure.com/courses/1297/assignments/265132?confetti=true&submitted=0
 
 //Select and cache the <main> element in a variable named mainEl.
 const mainEl = document.querySelector('main');
@@ -35,5 +29,45 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 //Add a class of flex-around to topMenuEl.
 topMenuEl.classList.add('flex-around');
 
-// 
+// Menu data structure
+const menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+// Iterate over the entire menuLinks array and for each "link" object:
+//menuLinks.forEach(element => { });
+/* menuLinks.forEach(obj => {
+    const a = document.createElement('a');
+    a.setAttribute('href', obj.href);
+    a.textContent = obj.text;
+    topMenuEl.appendChild(a);
+})
+*/
+
+// Create an <a> element.
+
+const links = menuLinks.map(obj => {
+    const a = document.createElement('a');
+    a.setAttribute('href', obj.href);
+    a.textContent = obj.text;
+    return a
+});
+
+console.log(links);
+
+const nav = document.getElementById('top-menu');
+links.forEach(a => topMenuEl.append(a)) 
+// topMenuEl.append(links);
+
+//console.log(menuLinks[0].text)
+// On the new element, add an href attribute with its value set to the href property of the "link" object.
+
+
+// Set the new element's content to the value of the text property of the "link" object.
+
+// Append the new element to the topMenuEl element.
+
 
